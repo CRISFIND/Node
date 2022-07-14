06104b2dcd7ad44b3d569e2d0a7be48e83669fe2
Cristhian <crisfind16@gmail.com>
2022-07-14 15:50:19 -0500

    Node

---
+++ app2.js
@@ -0,0 +1,11 @@
+// const saludar = (nombre) => {
+//     return `Saludo ${nombre}`;
+// }
+
+// console.log(saludar('Cristhian'));
+
+
+const animal = (color,raza) => {
+    return `Soy un Perro de raza ${raza} y de color ${color}`;
+}
+console.log(animal('negro','pitbull'));
